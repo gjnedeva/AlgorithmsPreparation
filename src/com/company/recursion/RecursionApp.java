e25fedaff94a6a19537f5e8219e73c3947793e9c
gnedeva <gnedeva@synchr.com>
2020-07-10 00:05:55 +0300

    AlgorithmsSoftUni

--- src/com/company/recursion/RecursionApp.java
+++ src/com/company/recursion/RecursionApp.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
 class RecursionApp {
- 	//	not use recursion for sum because recursion is heavier than for. It will copy the values.
+	//	not use recursion for sum because recursion is heavier than for. It will copy the values.
 	private static int Sum(int[] arr, int index) {
 		//put base case for stop it
 		if (index == arr.length) {
@@ -26,11 +26,11 @@ class RecursionApp {
 		if (index == 0) {
 			return;
 		}
-		System.out.println(StringUtils.repeat('*',index));
+		System.out.println(StringUtils.repeat('*', index));
 
 		Print(index - 1);
 
-		System.out.println(StringUtils.repeat('#',index));
+		System.out.println(StringUtils.repeat('#', index));
 
 
 	}
@@ -40,6 +40,33 @@ class RecursionApp {
 			return 1;
 		}
 		return num * Factorial(num - 1);
+	}
+
+	private static long IteralFactoria(int num) {
+		long result = 1;
+		for (int i = 1; i <= num; i++) {
+			result *= i;
+		}
+		return result;
+	}
+
+	private static int FibonacciRecursive(int n) {
+		if (n <= 1) {
+			return n;
+		} else {
+			return (FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2));
+		}
+	}
+
+	private static int FibonacciIterative(int n) {
+		int num1 = 0, num2 = 1;
+		int result = 0;
+		for (int i = 1; i < n; ++i) {
+			result = num1 + num2;
+			num1= num2;
+			num2 = result;
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
@@ -54,7 +81,12 @@ class RecursionApp {
 		int factorial = myInput.nextInt();
 		System.out.println(Factorial(factorial));
 
+		System.out.println("Print factorial with iteration:" + IteralFactoria(factorial));
 //      print * and # from recursion
 		Print(5);
+
+		/*expected result 34 */
+		System.out.println("Print Fibonacci: " + FibonacciRecursive(9));
+		System.out.println("Print Iterative: " + FibonacciIterative(9));
 	}
 }
